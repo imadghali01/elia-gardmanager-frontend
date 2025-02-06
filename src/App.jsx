7e7f61778ebe80ffbfe117d6c8bdeb956b8dc68f
Fauve <fauve.mareels@outlook.com>
2025-02-06 10:22:00 +0100

    Add: routes register and create page register

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/tailwind-global.css';
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 
 function App() {
 
   return (
-    <div>
-    <Route path="/login" element={<Login />} />
-  </div>
-  )
+    <BrowserRouter future={{ v7_startTransition: true }}>
+    <Routes>
+    <Route path="/" element={<h1 className="text-3xl font-bold text-center">Bienvenue sur Elia GardManager</h1>} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+    </BrowserRouter>
+)
+
+
 }
 
 export default App
