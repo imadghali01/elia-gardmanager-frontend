90b1a21faa8b1ea07e2bae5a1c76e9f274cf685e
Fauve <fauve.mareels@outlook.com>
2025-02-10 13:18:01 +0100

    feat: create Schedule page with reusable Navbar and add routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './styles/tailwind-global.css';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import HomeAdmin from "./pages/HomeAdmin";
+import Schedule from "./pages/Schedule";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/HomeAdmin" element={<HomeAdmin />} />
+      <Route path="/Schedule" element={<Schedule />} />
     </Routes>
     </BrowserRouter> 
 )
