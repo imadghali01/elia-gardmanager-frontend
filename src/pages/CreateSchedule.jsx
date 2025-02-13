d9635bce6346d714cbd4819e8419160b349ccbf5
Fauve <fauve.mareels@outlook.com>
2025-02-13 23:15:17 +0100

    feat: add schedule creation form in CreateSchedule component

--- src/pages/CreateSchedule.jsx
+++ src/pages/CreateSchedule.jsx
@@ -51,44 +51,115 @@ function CreateSchedule() {
         <div className="bg-white py-2 px-6 shadow-md rounded-lg">
 
         <div className="min-h-screen bg-white flex flex-col items-center">
+
+        <h1 className="text-xl">Create schedule</h1>    
         
         <form onSubmit={handleSubmit} className="space-y-6">
-        <div className="flex">
-        {/* Champ Full Name */}
-        <div className="w-2/3">
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            {/* Champ Full Name */}
+            <div className="flex space-x-4 w-full">
+            <div className="flex-1">
+                <label htmlFor="FullName" className="block text-sm font-medium text-gray-700">
                 Full Name
                 </label>
                 <input
-                id="email"
-                name="email"
+                id="FullName"
+                name="FullName"
                 placeholder="Full Name"
-                type="email"
+                type="text"
                 required
-                autoComplete="email"
                 value={formData.email}
                 onChange={handleChange}
                 className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
             </div>
-            {/* Champ contract */}
-            <div className="w-1/3 ml-4"> 
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                N° de contract
+            </div>
+
+            <div className="flex space-x-4">
+
+            {/* Champ StartDate */}
+            <div className="w-3/4">
+                <label htmlFor="Date" className="block text-sm font-medium text-gray-700">
+                
                 </label>
                 <input
-                id="email"
-                name="email"
-                placeholder="487690"
-                type="email"
+                id="Date"
+                name="Date"
+                placeholder="02/01/2020"
+                type="text"
                 required
-                autoComplete="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>
+
+            {/* Champ StartHours */}
+            <div className="w-2/4">
+                <label htmlFor="Hours" className="block text-sm font-medium text-gray-700">
+                
+                </label>
+                <input
+                id="Hours"
+                name="Hours"
+                placeholder="12:00 AM"
+                type="text"
+                required
+                value={formData.email}
+                onChange={handleChange}
+                className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>
+
+            
+
+            </div>
+
+            <div className="flex space-x-4">
+
+            {/* Champ EndDate */}
+            <div className="w-3/4">
+                <label htmlFor="Date" className="block text-sm font-medium text-gray-700">
+                
+                </label>
+                <input
+                id="Date"
+                name="Date"
+                placeholder="02/01/2020"
+                type="text"
+                required
+                value={formData.email}
+                onChange={handleChange}
+                className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>    
+
+            {/* Champ EndHours */}
+            <div className="w-2/4">
+                <label htmlFor="Hours" className="block text-sm font-medium text-gray-700">
+                
+                </label>
+                <input
+                id="Hours"
+                name="Hours"
+                placeholder="12:00 AM"
+                type="text"
+                required
                 value={formData.email}
                 onChange={handleChange}
                 className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
             </div>
         </div>
+
+            {/* Bouton de soumission */}
+            <div>
+                <button
+                type="submit"
+                className="w-full flex justify-center rounded-md bg-orange-500 px-4 py-2 text-white font-semibold shadow hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                >
+                Create Schedule
+                </button>
+            </div>
           </form>
         
         </div>
